f35d615e71a2c5416ec96702a32caa961376b718
Pedro Pinheiro <pedropaulocspinheiro@gmail.com>
2019-05-23 20:28:05 +0000

    feat(game): list of player types

--- src/main/java/domain/entities/Game.java
+++ src/main/java/domain/entities/Game.java
@@ -1,14 +1,24 @@
 package domain.entities;
 
+import domain.entities.characters.PlayerType;
+
+import java.util.List;
+
 public class Game {
 
     private Stage firstStage;
+    private List<PlayerType> playerTypes;
 
-    public Game(Stage firstStage) {
+    public Game(Stage firstStage, List<PlayerType> playerTypes) {
         this.firstStage = firstStage;
+        this.playerTypes = playerTypes;
     }
 
     public Stage getFirstStage() {
         return firstStage;
+    }
+
+    public List<PlayerType> getPlayerTypes() {
+        return playerTypes;
     }
 }
